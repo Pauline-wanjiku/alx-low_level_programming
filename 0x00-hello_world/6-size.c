86ee0f34353d6b62f38640476925b504cc99bf13
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-03-16 16:57:24 -0700

    made changes and used unsigned

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,10 @@
 int main(void)
 { int intType; long int longInt; long long int longlIntype; char charType; float floatType;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(intType));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(longInt));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longlIntype));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(charType));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n",sizeof(intType));
+printf("Size of an int: %zu byte(s)\n", sizeof(longInt));
+printf("Size of a long int: %zu byte(s)\n",sizeof(longlIntype));
+printf("Size of a long long int: %zu byte(s)\n",sizeof(charType));
+printf("Size of a float: %zu byte(s)\n",sizeof(floatType));
 return (0);
 } 
