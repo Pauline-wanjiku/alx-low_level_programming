0605b28f370788d2fd60fd66c3dc5ac0c9902a17
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-03-22 06:43:12 -0700

    fixed betty code style bug

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,16 @@
 
 int print_last_digit(int n)
 {
-        int lastDigit;
+	int lastDigit;
 
-        lastDigit = n % 10;
+	lastDigit = n % 10;
 
-        if (lastDigit < 0)
-        {
-                lastDigit *= -1;
-        }
-        _putchar(lastDigit + '0');
+	if (lastDigit < 0)
+	{
+		lastDigit *= -1;
+	}
 
-        return (lastDigit);
+	_putchar(lastDigit + '0');
+
+	return (lastDigit);
 }
