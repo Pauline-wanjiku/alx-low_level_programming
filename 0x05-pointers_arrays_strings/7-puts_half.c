e060e7ea71853b16f29c4149238cd58457845765
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-03-27 18:21:25 -0700

    solution to task  7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *puts_half - print the second half of the string
+ *@str : string to be printed
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int index, half;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+
+	half = index / 2;
+
+	if (index % 2 == 1)
+		half++;
+
+	while (half < index)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
