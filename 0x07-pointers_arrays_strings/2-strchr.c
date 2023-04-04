3d4800eaf1330d5204b2c1d08691ab35118a976a
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-04 12:33:50 -0700

    fixed the bug

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,28 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-
 /**
- *_strchr -  function that locates a character in a string.
- *@s:the string
- *@c:the character
- *Return:pointer to the first occurrence of the character
+_strchr - Entry point
+@s: input
+@c: input
+Return: Always 0 (Success)
  */
-
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	int i = 0;
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-		{
 			return (&s[i]);
-		}
-	}
-	if (c == '\0')
-	{
-		return (s);
 	}
 	return (0);
 }
