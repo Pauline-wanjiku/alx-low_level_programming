205b48abe874d546e651799196f2065b1bf0d171
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-05 18:57:41 -0700

    fixed betty style issue

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 
 /**
  * is_prime_number-  function that returns 1 or 0 if n is prime number
- *@n - the input value
+ *@n: the input value
  * Return: Always 0.
  */
 
@@ -13,14 +13,14 @@ int is_prime_number(int n)
 
 	if (n <= 1)
 	{
-		return(0);
-	}
-	 for (i = 2; i * i <= n; i++) 
-	 {
-		 if (n % i == 0) 
-		 {
-			 return 0;
-		 }
-	 }
-	 return 1;
+		return (0);
+	{
+		for (i = 2; i * i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				return (0);
+			}
+		}
+		return (1);
 }
