e6b08391ab3b84ebab090a62703b2d665d83ea9f
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-11 10:07:09 -0700

    function that concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *str_concat - concatenates two strings.
+ *@s1: string 1
+ *@s2:string2
+ *Return: null on fail
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned int s1Length = 0;
+	unsigned int s2Length = 0;
+	unsigned int s3Length;
+       	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s1Length = 0;
+	while (s1[s1Length])
+		s1Length++;
+	while (s2[s2Length])
+		s2Length++;
+	s3Length = s1Length + s2Length;
+	s3 = malloc(sizeof(char) * s3Length + 1);
+	if (s3 == NULL)
+		return (NULL);
+	while (i < s1Length)
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (i <= s3Length)
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	return (s3);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -19,3 +19,13 @@ function that returns a pointer to a newly allocated space in memory, which cont
 
 [solution](/0x0B-malloc_free/1-strdup.c)
 
+#### 2. He who is not courageous enough to take risks will accomplish nothing in life
+function that concatenates two strings.
+
+- Prototype: char *str_concat(char *s1, char *s2);
+- The returned pointer should point to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated
+- if NULL is passed, treat it as an empty string
+- The function should return NULL on failure
+
+[solution](/0x0B-malloc_free/2-str_concat.c)
+
