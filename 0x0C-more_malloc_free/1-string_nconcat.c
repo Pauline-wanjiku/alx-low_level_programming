49ad59fd5fb10e96357b0fa27120b298959dd774
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-13 16:18:47 -0700

    fixed thr bug on the output

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,32 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
  *string_nconcat-function that concatenates two strings.
  *@s1:string 1
  *@s2:string 2
- *@n:size 
+ *@n:size
  *Return:pointer which contains 2
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i, j, length_of_s1 = 0, length_of_s2 = 0;
 	char *ptr;
-
-	size_t length_of_s1 = strlen(s1);
-	size_t length_of_s2 = strlen(s2);
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= length_of_s2)
-		n = length_of_s2;
-	ptr = malloc(length_of_s1 + n + 1);
+	while (s1 && s1[length_of_s1])
+		length_of_s1++;
+	while (s2 && s2[length_of_s2])
+		length_of_s2++;
+	if (n < length_of_s2)
+		ptr = malloc(sizeof(char) * (length_of_s1 + n + 1));
+	else
+		ptr = malloc(sizeof(char) * (length_of_s1 + length_of_s2 + 1));
+
 	if (ptr == NULL)
 		return (NULL);
 	i = 0;
@@ -33,7 +36,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	j = 0;
-	while (n < length_of_s2 && i < (length_of_s2 + n))
+	while (n < length_of_s2 && i < (length_of_s1 + n))
+		ptr[i++] = s2[j++];
+
+	while (n >= length_of_s2 && i < (length_of_s1 + length_of_s2))
 		ptr[i++] = s2[j++];
 	ptr[i] = '\0';
 	return (ptr);
