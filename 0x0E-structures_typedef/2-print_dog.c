edea2a0b5a3a4be52fcf98fa373cc4bf4dc48078
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-17 07:10:36 -0700

    fixed betty bug

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,10 +3,10 @@
 
 /**
  *print_dog-function that prints a struct dog
- *@d - intialize variable
+ *@d:intialize variable
  */
 
-void print_dog(struct dog *d)	
+void print_dog(struct dog *d)
 {
 	if (!d)
 	{
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name)
 	{
-		printf("Name: %s\n", d->name);		
+		printf("Name: %s\n", d->name);
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner)
 	{
-		printf("Owner: %s\n",d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 	else
 	{
