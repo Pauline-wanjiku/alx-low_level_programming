15b5ff7055d121d9c0c1b15094713569acfc3378
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-18 03:20:23 -0700

    fixed the double incursion

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3-FUNCTION_LIKE_MACRO_H
-#define _3-FUNCTION_MACRO_H
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_MACRO_H_
 #define ABS(x) ((x >= 0) ? (x) : -(x))
-#endif /* _3-FUNCTION_MACRO_H */
+#endif /* _3_FUNCTION_MACRO_H_ */
