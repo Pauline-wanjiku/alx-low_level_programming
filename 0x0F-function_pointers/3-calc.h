32fdd24c18dae8fbcece186a8538a942d8608540
Pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-04-20 08:01:36 -0700

    fixed bugs in task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -6,6 +6,7 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 /**
  * struct op - Struct op
  *
@@ -15,8 +16,8 @@ int (*get_op_func(char *s))(int, int);
 
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 #endif /* _3_CALC_H */
